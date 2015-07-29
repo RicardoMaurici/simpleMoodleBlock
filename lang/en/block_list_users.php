0ef6062db639cedf70e24b717216f9f6e446079b
Ricardo Ferreira <ricardo.maurici@hotmail.com>
2015-07-29 16:35:17 -0300

    bootstrap

---
+++ lang/en/block_list_users.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Strings for component 'block_list_users', language 'en'
+ *
+ * @package   block_list_users
+ * @copyright Ricardo Ferreira
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['blockstring'] = 'Block string';
+$string['descconfig'] = 'Description of the config section';
+$string['descfoo'] = 'Config description';
+$string['headerconfig'] = 'Config section header';
+$string['labelfoo'] = 'Config label';
+$string['list_users'] = 'List Users';
+$string['list_users:addinstance'] = 'Add a list usersblock';
+$string['list_users:myaddinstance'] = 'Add a list users block to my moodle';
+$string['pluginname'] = 'List Users Block';
